69c97e0ee62b80b739f87a2d395be04a3b085a30
Kartiknishu <kartiknishu2002@gmail.com>
2025-03-27 10:18:55 +0530

    commit 7

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -86,7 +86,7 @@ const Portfolio = () => {
   onChange={(e) => setSearchQuery(e.target.value)}
 />
         </div>
-        <div className="flex space-x-2 ">
+        <div className="flex space-x-2 items-center">
           {/* Column Selection */}
           <FilterSection
             selectedColumns={selectedColumns}
@@ -95,7 +95,7 @@ const Portfolio = () => {
           />
 
           {/* More Filters Button */}
-          <div className="flex items-center ml-2 mr-6">
+          <div className="flex items-center ml-2 mr-8">
           <button
             className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md"
             onClick={() => setIsFiltersOpen(true)}
