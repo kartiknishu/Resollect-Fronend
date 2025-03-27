27d679d3999016f7f35500bcae91f74f2d57293a
Kartiknishu <kartiknishu2002@gmail.com>
2025-03-27 09:56:03 +0530

    commit 4

--- src/components/UploadSidebar.jsx
+++ src/components/UploadSidebar.jsx
@@ -39,7 +39,7 @@ const UploadSidebar = ({ isOpen, onClose }) => {
 
   return (
     <div
-      className={`fixed top-0 right-0 w-96 h-full bg-white shadow-lg transition-transform transform ${
+      className={`fixed top-0 right-0 h-full bg-white shadow-lg transition-transform transform ${
         isOpen ? "translate-x-0" : "translate-x-full"
       } z-50 p-6`}
     >
